93819e2443a89102d3f538b9fac343d159e32217
Ana Bebic <anabebic.x@gmail.com>
2018-08-03 20:49:54 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,7 +2,7 @@ module Players
   class Computer < Player
     
     def move 
-      includef board.cells.count{|square| square != " " } == 0
+      if board.cells.count{|square| square != " " } == 0
         "1"
       elsif board.cells.count{|square| square != " " } == 1
         board.cells.find_index("X") == 4 ? "1" : "5"
@@ -18,4 +18,3 @@ module Players
     end 
   end
   
-end
